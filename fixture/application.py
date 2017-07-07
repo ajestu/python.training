1872a2a3be7d6bb3737aa72ccde5e83f759ee3ff
evgeny <ajestu@gmail.com>
2017-07-07 21:12:15 +0300

    добавлены фикстуры.. разбито на вспомогательные классы

---
+++ fixture/application.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import session_helper
+from fixture.group import Group_helper
+
+class Application:
+
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+        self.session = session_helper(self)
+        self.group = Group_helper(self)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/group.py
@@ -0,0 +1,31 @@
+class Group_helper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("model page").click()
+
+    def create(self, group):
+        wd = self.app.wd
+        self.app.open_home_page()
+        # init model creation
+        wd.find_element_by_name("new").click()
+        # fill model form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit model creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
