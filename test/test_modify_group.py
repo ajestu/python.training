fe73a14571c94cab6e0fc6080317b526c9a75401
evgeny <ajestu@gmail.com>
2017-07-12 20:37:44 +0300

    таки реализована проверка валидности фикстуры иутекущего состояния сессии

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,10 +3,9 @@ from model.group import Group
 def test_modify_group_name(app):
     app.group.modify(Group(name="New name"))
 
-
-# def test_modify_group_header(app):
- #   app.group.modify_first_group(Group(header="New header"))
+def test_modify_group_header(app):
+    app.group.modify(Group(header="New header"))
 
 
 #def test_modify_group_footer(app):
- #   app.group.modify_first_group(Group(footer="New footer"))
+ #   app.group.modify(Group(footer="New footer"))
