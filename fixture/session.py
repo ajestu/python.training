090bbbe689537ea0fa5295bd0c9ab43afa7ef072
evgeny <ajestu@gmail.com>
2017-07-26 13:36:26 +0300

    задание 16

--- fixture/session.py
+++ fixture/session.py
@@ -4,6 +4,7 @@ class session_helper:
     def __init__(self, app):
         self.app = app
 
+
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
